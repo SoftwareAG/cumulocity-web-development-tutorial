992fd6008b65241c2abeb43afaf39463e976b268
Christian Guether <christian.guether@softwareag.com>
2023-01-03 10:23:10 +0100

    add realtime measurements

--- part-03/my-c8y-application/src/device-info/device-info.service.ts
+++ part-03/my-c8y-application/src/device-info/device-info.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, resolveForwardRef } from '@angular/core';
-import { InventoryService, MeasurementService } from '@c8y/client';
-import { MeasurementRealtimeService } from '@c8y/ngx-components';
+import { IMeasurement, InventoryService, MeasurementService, Realtime } from '@c8y/client';
 import { Subject } from 'rxjs';
 import { DeviceDetails, TemperatureMeasuerement } from './device-info.model';
 import { has, get } from 'lodash';
@@ -16,7 +15,8 @@ export class DeviceInfoService {
 
   constructor(
     private inventoryService: InventoryService,
-    private measurementService: MeasurementService
+    private measurementService: MeasurementService,
+    private realtime: Realtime
   ) {}
 
   async getDeviceDetails(deviceId: string): Promise<DeviceDetails> {
@@ -34,6 +34,8 @@ export class DeviceInfoService {
 
   subscribeForTemperatureMeasurements(deviceId: string): void {
     this.loadLatestMeasurement(deviceId, this.TEMPERATURE_FRAGMENT, this.TEMPERATURE_SERIES);
+
+    this.subscribeForMeasurements(deviceId, this.TEMPERATURE_FRAGMENT, this.TEMPERATURE_SERIES);
   }
 
   private async loadLatestMeasurement(
@@ -75,5 +77,28 @@ export class DeviceInfoService {
     } catch (error) {
       console.error('Error occurred while loading the latest measurement: ', error);
     }
+  }
+
+  private subscribeForMeasurements(
+    deviceId: string,
+    measurementFragment: string,
+    measurementSeries: string
+  ) {
+    this.realtime.subscribe(`/measurements/${deviceId}`, (measurementNotification) => {
+      const measurement: IMeasurement = measurementNotification.data.data;
+      if (!measurement || !has(measurement, `${measurementFragment}.${measurementSeries}`)) {
+        return;
+      }
+
+      const temperatureValue: number = get(
+        measurement,
+        `${measurementFragment}.${measurementSeries}.value`
+      );
+      const temperatureUnit: string = get(
+        measurement,
+        `${measurementFragment}.${measurementSeries}.unit`
+      );
+      this.temperatureMeasurement$.next({ value: temperatureValue, unit: temperatureUnit });
+    });
   }
 }
