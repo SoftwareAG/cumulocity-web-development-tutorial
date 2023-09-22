f1256bffc1c3ab783fb0f186cb642dfc8b23bbf9
Christian Guether <christian.guether@softwareag.com>
2023-09-22 14:09:52 +0200

    #7 replace injection tokens in part-04

--- part-04/my-c8y-application/src/named-dashboard/named-dashboard.module.ts
+++ part-04/my-c8y-application/src/named-dashboard/named-dashboard.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CoreModule, HOOK_NAVIGATOR_NODES } from '@c8y/ngx-components';
+import { CoreModule, hookNavigator } from '@c8y/ngx-components';
 import { ContextDashboardModule } from '@c8y/ngx-components/context-dashboard';
 import { NamedDashboardComponent } from './named-dashboard.component';
 import { NamedDashboardNavigationFactory } from './named-dashboard.factory';
@@ -28,8 +28,6 @@ const routes: Routes = [
   ],
   exports: [],
   declarations: [NamedDashboardComponent],
-  providers: [
-    { provide: HOOK_NAVIGATOR_NODES, useClass: NamedDashboardNavigationFactory, multi: true },
-  ],
+  providers: [hookNavigator(NamedDashboardNavigationFactory)],
 })
 export class NamedDashboardModule {}
