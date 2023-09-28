8972a75a3a6356a99d151e52337571ef4d762471
Christian Guether <57527184+CGuether@users.noreply.github.com>
2023-09-28 15:17:10 +0200

    #9 Replace Realtime service with MeasurementRealtimeService (#10)
    * #9 replace realtime service with measurementRealtimeService in part 03
    
    * #9 replace realtime service with MeasurementRealtimeService in part 04
    
    * #9 replace realtime service with MeasurementRealtimeService in part 05

--- part-03/my-c8y-application/src/device-info/device-info.module.ts
+++ part-03/my-c8y-application/src/device-info/device-info.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CoreModule, hookNavigator } from '@c8y/ngx-components';
+import { CoreModule, MeasurementRealtimeService, hookNavigator } from '@c8y/ngx-components';
 
 import { DeviceInfoComponent } from './device-info.component';
 import { DeviceInfoNavigationFactory } from './device-info.factory';
@@ -21,6 +21,6 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes), CoreModule],
   exports: [],
   declarations: [DeviceInfoComponent],
-  providers: [hookNavigator(DeviceInfoNavigationFactory)],
+  providers: [MeasurementRealtimeService, hookNavigator(DeviceInfoNavigationFactory)],
 })
 export class DeviceInfoModule {}
--- part-05/my-first-plugin/modules/device-info/device-info.module.ts
+++ part-05/my-first-plugin/modules/device-info/device-info.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
-import {
-  CoreModule,
-  hookComponent,
-} from '@c8y/ngx-components';
+import { CoreModule, MeasurementRealtimeService, hookComponent } from '@c8y/ngx-components';
 import { ContextWidgetConfig } from '@c8y/ngx-components/context-dashboard';
 import { DeviceInfoComponent } from './device-info.component';
 
@@ -11,6 +8,7 @@ import { DeviceInfoComponent } from './device-info.component';
   exports: [],
   declarations: [DeviceInfoComponent],
   providers: [
+    MeasurementRealtimeService,
     hookComponent({
       id: 'device-info.widget',
       label: 'Device Info Widget',
