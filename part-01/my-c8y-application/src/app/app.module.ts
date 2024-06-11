1ee09fee574bba779766a601ebfa7184623b8725
Christian Guether <christian.guether@softwareag.com>
2024-06-11 09:37:05 +0200

    #11 update part 01 to angular cli and new c8y web sdk

--- part-01/my-c8y-application/src/app/app.module.ts
+++ part-01/my-c8y-application/src/app/app.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule as ngRouterModule } from '@angular/router';
 import { CoreModule, BootstrapComponent, RouterModule } from '@c8y/ngx-components';
 
 @NgModule({
-  imports: [
-    BrowserAnimationsModule,
-    RouterModule.forRoot(),
-    ngRouterModule.forRoot([], { enableTracing: false, useHash: true }),
-    CoreModule.forRoot()
-  ],
+  imports: [BrowserAnimationsModule, RouterModule.forRoot([]), CoreModule.forRoot()],
   bootstrap: [BootstrapComponent]
 })
 export class AppModule {}
